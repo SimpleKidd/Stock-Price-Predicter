1a56f94776e9a13c23d395292651c6025645e313
ashuashutosh2211 <111591264+ashuashutosh2211@users.noreply.github.com>
2023-10-25 10:26:17 +0530

    Update 📈_Homepage.py

--- 📈_Homepage.py
+++ 📈_Homepage.py
@@ -1,21 +1,24 @@
 import streamlit as st
-import json
 from streamlit_lottie import st_lottie
+import requests
 
 st.set_page_config(
-    page_title="💸Homepage"
+    page_title="💸Homepage",
+    initial_sidebar_state="expanded",
+    layout="wide"
 )
 
-def load_lottiefile(filepath: str):
-    with open(filepath,"r") as f:
-        return json.load(f)
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
 col1,col2=st.columns(2)
 
-with col2:
-    lottie_stocks=load_lottiefile("pages\stocks.json")  # replace link to local lottie file
-    st_lottie(lottie_stocks,height=300,width=400)
 with col1:
-    st.title('Welcome to StockSmart!')
-    st.write('\n\n\n')
-    st.write("Presenting StockSmart - The Golden Path to Stock Prognostication.We are here to assist you every step of the way, providing invaluable insights and empowering you to make informed decisions. With our cutting-edge tools and comprehensive resources, we enable you to navigate the dynamic stock market landscape with confidence. Unleash the power of prediction as we unravel the opening range of stock prices, granting you a glimpse into the exciting realm of market trends.")
+    st.title('Welcome to Stock Price Predictor!')
+    st.write("Presenting Stock Price Predictor - The Golden Path to Stock Prognostication.We are here to assist you every step of the way, providing invaluable insights and empowering you to make informed decisions. Additionally, our expertise extends to predicting closing prices, equipping you with a strategic advantage in optimizing your investment strategies. Let us be your trusted companion on this thrilling journey, as we empower you to unlock the full potential of your financial aspirations.")
+with col2:
+    stocks=load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_kuhijlvx.json")
+    st_lottie(stocks)
